84e509fcabeb13ee506258937772653bc7d1cb5a
Wei Dai <me@wdai.us>
2022-10-15 11:10:25 -0500

    Add sketch wallet contract

---
+++ contracts/src/Wallet.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+struct Action {
+   address targetAddress;
+   bytes encodedCall;
+}
+
+contract Wallet {
+
+	function exec(calldata Action[] actions) {
+		if (msg.caller == NFTMaster.ownerOf(uint256 this)) {
+			for (uint256 i = 0; i < actions.length; i++) {
+        results[i] = _Call(actions[i]);
+      }
+		}
+	}
+
+	function _Call(Action calldata action)
+        internal
+        returns (bytes memory)
+    {
+        (bool success, bytes memory result) = action.targetAddress.call(
+            action.encodedCall
+        );
+
+        require(success, "Call failed");
+        return result;
+    }
+}
+
