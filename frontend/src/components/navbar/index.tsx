88a46e215da8805b32535281429191b69d402047
Will Jiang <willjiang2018@gmail.com>
2022-10-15 14:13:42 -0500

    wallet function done

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,18 +1,13 @@
-import Button from 'components/button'
-import classNames from 'classnames'
-import { ConnectKitButton } from 'connectkit'
-import { buildJazziconDataUrl } from 'helpers/jazzicon'
-import { useNavigate } from 'react-router-dom'
-import { useAccount, useDisconnect } from 'wagmi'
-import styles from './index.module.scss'
-import logo from './logo.png'
+import classNames from "classnames";
+import { ConnectKitButton } from "connectkit";
+import { useNavigate } from "react-router-dom";
+import styles from "./index.module.scss";
+import logo from "./logo.png";
 
 export interface NavbarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Navbar = ({ className, ...props }: NavbarProps) => {
-  const navigate = useNavigate() 
-  const { address } = useAccount()
-  const { disconnect } = useDisconnect()
+  const navigate = useNavigate();
 
   return (
     <div className={classNames(styles.navbar, className)} {...props}>
@@ -22,19 +17,7 @@ const Navbar = ({ className, ...props }: NavbarProps) => {
         alt="Sell Your Soul Wallet"
         onClick={() => navigate("/")}
       />
-      {address ? (
-        <div>
-          <img
-            className={styles.jazzicon}
-            src={buildJazziconDataUrl(address)}
-            alt={address}
-            onClick={() => navigate(`/users/${address}`)}
-          />
-          <Button onClick={() => disconnect()}>Disconnect</Button>
-        </div>
-      ) : (
-        <ConnectKitButton />
-      )}
+      <ConnectKitButton />
     </div>
   );
 };
