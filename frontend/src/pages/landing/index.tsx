c00e8eef3e4771595b62ec3d0b520ac19e0b42fa
Feng Yan <fy48@duke.edu>
2022-10-15 18:08:54 -0500

    merge

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -56,14 +56,9 @@ const LandingPage = () => {
         <div className={styles.heading}>Sell Your Soul Wallet is ....</div>
         <div className={styles.description}>This is ...</div>
         <div className={styles.cards}>
-          <div onClick={() => alert("Clicked Mint")}>
+          <div onClick={() => mintNFTWallet(signer, toast)}>
             <Card className={styles.card}>
-              <button
-                className={styles.button5}
-                onClick={() => mintNFTWallet(signer, toast)}
-              >
-                +
-              </button>
+              <button className={styles.button5}>+</button>
             </Card>
           </div>
           {NFT_Wallets.map(({ id, description }) => (
