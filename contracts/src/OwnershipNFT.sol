4129e116b72a1474dee477822f36666f14bdac5b
rabib <53358432+rabib@users.noreply.github.com>
2022-10-15 12:53:29 -0500

    impl OwnershipNFT

--- contracts/src/OwnershipNFT.sol
+++ contracts/src/OwnershipNFT.sol
@@ -1,5 +1,19 @@
 pragma solidity ^0.8.13;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "./Wallet.sol";
 
-contract OwnershipNFT is ERC721 {}
+contract OwnershipNFT is ERC721 {
+
+    constructor(
+        string memory name_, 
+        string memory symbol_) 
+        ERC721(name_, symbol_) {}
+
+    function mint() external {
+        address wallet = new Wallet();
+        uint256 tokenId = uint256(wallet);
+        _mint(msg.sender, tokenId);
+        return tokenId;
+    }
+}
